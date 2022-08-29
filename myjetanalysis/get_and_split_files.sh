23addb1a2086552999d29228cee57731e0cfb5f8
Veronica Verkest <vverkest@gmail.com>
2022-08-29 15:40:29 -0400

    updated to have subdirectories in lists, out, and condor-out for "calo_rho" and "jet_bg"

--- myjetanalysis/get_and_split_files.sh
+++ myjetanalysis/get_and_split_files.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 
-mkdir -p out condor-out
+mkdir -p out condor-out/jet_bg out/jet_bg condor-out/calo_rho out/calo_rho
 
-mkdir -p lists
+mkdir -p lists lists/calo_rho lists/jet_bg
 
-cd lists
+cd lists/calo_rho
 
 CreateFileList.pl -run 4 -type 11 -embed DST_BBC_G4HIT DST_CALO_CLUSTER DST_CALO_G4HIT DST_TRUTH_JET DST_VERTEX
 
 split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_truth_jet.list "dst_truth_jet_"
+split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_calo_g4hit.list "dst_calo_g4hit_"
+split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_calo_cluster.list "dst_calo_cluster_"
+split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_vertex.list "dst_vertex_"
+split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_bbc_g4hit.list "dst_bbc_g4hit_"
+
+if [ -f index.txt ]
+then
+    rm index.txt
+fi
+
+for i in {1..75..1}
+do
+  printf "%0*d\n" 2 $i >> index.txt
+done
+
+cd lists/jet_bg
+
+CreateFileList.pl -run 4 -type 4 DST_BBC_G4HIT DST_CALO_CLUSTER DST_CALO_G4HIT DST_VERTEX
+
 split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_calo_g4hit.list "dst_calo_g4hit_"
 split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_calo_cluster.list "dst_calo_cluster_"
 split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_vertex.list "dst_vertex_"
