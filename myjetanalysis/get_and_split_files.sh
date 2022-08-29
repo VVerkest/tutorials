c6a9814be414772dee19a5ef0f237b82264ca59d
Veronica Verkest <vverkest@gmail.com>
2022-08-29 16:59:15 -0400

    fix jet_bg file splitting!

--- myjetanalysis/get_and_split_files.sh
+++ myjetanalysis/get_and_split_files.sh
@@ -28,17 +28,17 @@ cd lists/jet_bg
 
 CreateFileList.pl -run 4 -type 4 DST_BBC_G4HIT DST_CALO_CLUSTER DST_CALO_G4HIT DST_VERTEX
 
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_calo_g4hit.list "dst_calo_g4hit_"
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_calo_cluster.list "dst_calo_cluster_"
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_vertex.list "dst_vertex_"
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_bbc_g4hit.list "dst_bbc_g4hit_"
+split -l100 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_calo_g4hit.list "dst_calo_g4hit_"
+split -l100 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_calo_cluster.list "dst_calo_cluster_"
+split -l100 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_vertex.list "dst_vertex_"
+split -l100 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_bbc_g4hit.list "dst_bbc_g4hit_"
 
 if [ -f index.txt ]
 then
     rm index.txt
 fi
 
-for i in {1..75..1}
+for i in {1..400..1}
 do
-  printf "%0*d\n" 2 $i >> index.txt
+  printf "%0*d\n" 3 $i >> index.txt
 done
