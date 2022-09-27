42a2090e8dbbf2a9c9a861aecce932ebdb116b97
Veronica Verkest <vverkest@gmail.com>
2022-09-27 19:41:14 -0400

    10 and 30 GeV jet options

--- myjetanalysis/get_and_split_files.sh
+++ myjetanalysis/get_and_split_files.sh
@@ -1,60 +1,117 @@
 #!/bin/sh
 
 mkdir -p condor-out condor-out/jet_bg condor-out/calo_rho condor-out/no_embed condor-out/trk condor-out/no_embed
-
+mkdir -p condor-out/no_embed/10GeV condor-out/no_embed/30GeV condor-out/calo_rho/10GeV condor-out/calo_rho/30GeV
+mkdir -p out/calo_rho/10GeV out/calo_rho/30GeV out/calo_rho/10GeV out/calo_rho/30GeV
 mkdir -p lists lists/calo_rho lists/jet_bg lists/trk lists/no_embed
-
 mkdir -p out out/trk out/jet_bg out/calo_rho out/no_embed
 
 DIR=$(cd "$(dirname "$0")"; pwd)
 
-cd lists/calo_rho
+
+
+cd lists/calo_rho/30GeV
 
 echo "entering directory `pwd`"
 
 CreateFileList.pl -run 40 -type 11 -embed DST_BBC_G4HIT DST_CALO_CLUSTER DST_CALO_G4HIT DST_TRUTH_JET DST_VERTEX
 
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_truth_jet.list "dst_truth_jet_"
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_calo_g4hit.list "dst_calo_g4hit_"
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_calo_cluster.list "dst_calo_cluster_"
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_vertex.list "dst_vertex_"
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_bbc_g4hit.list "dst_bbc_g4hit_"
+split -l50 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_truth_jet.list "dst_truth_jet_"
+split -l50 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_calo_g4hit.list "dst_calo_g4hit_"
+split -l50 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_calo_cluster.list "dst_calo_cluster_"
+split -l50 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_vertex.list "dst_vertex_"
+split -l50 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_bbc_g4hit.list "dst_bbc_g4hit_"
 
 if [ -f index.txt ]
 then
     rm index.txt
 fi
 
-for i in {1..75..1}
+for i in {1..300..1}
 do
-  printf "%0*d\n" 2 $i >> index.txt
+  printf "%0*d\n" 3 $i >> index.txt
 done
 
 cd $DIR
 
-cd lists/no_embed
+
+
+cd lists/calo_rho/10GeV
+
+echo "entering directory `pwd`"
+
+CreateFileList.pl -run 40 -type 12 -embed DST_BBC_G4HIT DST_CALO_CLUSTER DST_CALO_G4HIT DST_TRUTH_JET DST_VERTEX
+
+split -l5 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_truth_jet.list "dst_truth_jet_"
+split -l5 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_calo_g4hit.list "dst_calo_g4hit_"
+split -l5 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_calo_cluster.list "dst_calo_cluster_"
+split -l5 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_vertex.list "dst_vertex_"
+split -l5 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_bbc_g4hit.list "dst_bbc_g4hit_"
+
+if [ -f index.txt ]
+then
+    rm index.txt
+fi
+
+for i in {1..300..1}
+do
+  printf "%0*d\n" 3 $i >> index.txt
+done
+
+cd $DIR
+
+
+cd lists/no_embed/30GeV
 
 echo "entering directory `pwd`"
 
 CreateFileList.pl -run 40 -type 11 DST_BBC_G4HIT DST_CALO_CLUSTER DST_CALO_G4HIT DST_TRUTH_JET DST_VERTEX
 
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_truth_jet.list "dst_truth_jet_"
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_calo_g4hit.list "dst_calo_g4hit_"
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_calo_cluster.list "dst_calo_cluster_"
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_vertex.list "dst_vertex_"
-split -l40 --numeric-suffixes=1 --suffix-length=2 --additional-suffix=".list" dst_bbc_g4hit.list "dst_bbc_g4hit_"
+split -l50 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_truth_jet.list "dst_truth_jet_"
+split -l50 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_calo_g4hit.list "dst_calo_g4hit_"
+split -l50 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_calo_cluster.list "dst_calo_cluster_"
+split -l50 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_vertex.list "dst_vertex_"
+split -l50 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_bbc_g4hit.list "dst_bbc_g4hit_"
 
 if [ -f index.txt ]
 then
     rm index.txt
 fi
 
-for i in {1..75..1}
+for i in {1..300..1}
 do
-  printf "%0*d\n" 2 $i >> index.txt
+  printf "%0*d\n" 3 $i >> index.txt
 done
 
 cd $DIR
+
+
+
+cd lists/no_embed/10GeV
+
+echo "entering directory `pwd`"
+
+CreateFileList.pl -run 40 -type 12 DST_BBC_G4HIT DST_CALO_CLUSTER DST_CALO_G4HIT DST_TRUTH_JET DST_VERTEX
+
+split -l10 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_truth_jet.list "dst_truth_jet_"
+split -l10 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_calo_g4hit.list "dst_calo_g4hit_"
+split -l10 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_calo_cluster.list "dst_calo_cluster_"
+split -l10 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_vertex.list "dst_vertex_"
+split -l10 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_bbc_g4hit.list "dst_bbc_g4hit_"
+
+if [ -f index.txt ]
+then
+    rm index.txt
+fi
+
+for i in {1..150..1}
+do
+  printf "%0*d\n" 3 $i >> index.txt
+done
+
+cd $DIR
+
+
 
 cd lists/jet_bg
 
@@ -77,20 +134,8 @@ do
   printf "%0*d\n" 3 $i >> index.txt
 done
 
-#split -l100 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_calo_g4hit.list "dst_calo_g4hit_"
-#split -l100 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_calo_cluster.list "dst_calo_cluster_"
-#split -l100 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_vertex.list "dst_vertex_"
-#split -l100 --numeric-suffixes=1 --suffix-length=3 --additional-suffix=".list" dst_bbc_g4hit.list "dst_bbc_g4hit_"
-#
-#if [ -f index.txt ]
-#then
-#    rm index.txt
-#fi
-#
-#for i in {1..400..1}
-#do
-#  printf "%0*d\n" 3 $i >> index.txt
-#done
+
+
 
 cd $DIR
 
@@ -124,3 +169,12 @@ done
 #not reading pre-processed
 
 #run 4: different GEANT version
+
+
+#for i in {1..800..1}
+#do
+#  printf "%0*d\n" 3 $i >> index.txt
+#done
+
+
+# rm dst_vertex_*.list dst_truth_jet_*.list dst_calo_g4hit_*.list dst_calo_cluster_*.list dst_bbc_g4hit_*.list
